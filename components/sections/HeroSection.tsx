017ea383970a018b60f09c5184a9f90039f6def9
richmond-andoh <richmondandoh00@gmail.com>
2025-10-27 22:19:12 +0000

    Feat: Updates with fetching data from sanity

--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -1,6 +1,27 @@
-import React from "react";
+import { sanityFetch } from "@/sanity/lib/live";
+import { defineQuery } from "next-sanity";
 
-const HeroSection = () => {
+const HERO_QUERY = defineQuery(`*[_id == "singleton-profile"][0]{
+  firstName,
+  lastName,
+  headline,
+  headlineStaticText,
+  headlineAnimatedWords,
+  headlineAnimationDuration,
+  shortBio,
+  email,
+  phone,
+  location,
+  availability,
+  socialLinks,
+  yearsOfExperience,
+  profileImage
+}`);
+
+const HeroSection = async () => {
+  const { data: profile } = await sanityFetch({ query: HERO_QUERY });
+  console.log(profile);
+
   return <div>Hero</div>;
 };
 
